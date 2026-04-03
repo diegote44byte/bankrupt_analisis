4fa3f3ec4cc919769da92a66d10f8769fa52f060
Diego Tumba <a20203402@pucp.edu.pe>
2026-04-03 12:20:51 -0500

    Update predict_fall.py

--- predict_fall.py
+++ predict_fall.py
@@ -1,14 +1,3 @@
-import pandas as pd
-import numpy as np
-import matplotlib.pyplot as plt
-import seaborn as sns
-import random
-
-
-def genepassword():
-    word = ["diego","adriano","david","judith"]
-    choose = word.sort(1)
-    print(f'escogiendo password' + choose )
 
 
 
